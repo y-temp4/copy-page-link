3f06e65206b94adaaf3fae1f8801b231015abc56
y-temp4 <y.temp4@gmail.com>
2022-06-14 21:47:05 +0900

    Implement copy title and url feature

---
+++ src/background.ts
@@ -0,0 +1,56 @@
+type Command = "copyDefault" | "copyMd";
+
+function injectedScript(command: Command, title: string, url: string) {
+  function copy(plain: string) {
+    const listener = (event: ClipboardEvent) => {
+      event.clipboardData?.setData("text/plain", plain);
+      event.preventDefault();
+    };
+    document.addEventListener("copy", listener);
+    document.execCommand("copy");
+    document.removeEventListener("copy", listener);
+  }
+  const copyText =
+    command === "copyDefault" ? `${title} ${url}` : `[${title}](${url})`;
+  copy(copyText);
+}
+
+chrome.commands.onCommand.addListener((cmd) => {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    async (tabs) => {
+      const command = cmd as Command;
+      async function run() {
+        try {
+          // console.log("tabs", tabs);
+          const [tab] = tabs;
+          const isAccessRestrictedUrl = !!tab.url?.startsWith("chrome://");
+          if (isAccessRestrictedUrl) return;
+          await chrome.scripting.executeScript({
+            target: { tabId: tab.id ?? 0 },
+            func: injectedScript,
+            // func: injectedScript(command, tab.title ?? "", tab.url ?? ""),
+            args: [command, tab.title ?? "", tab.url ?? ""],
+          });
+          const text = command === "copyDefault" ? "Plain" : "MD";
+          const color = command === "copyDefault" ? "#bac8ff" : "#b2f2bb";
+          await chrome.action.setBadgeBackgroundColor({ color });
+          await chrome.action.setBadgeText({ text });
+        } catch (error) {
+          console.error(error);
+          await chrome.action.setBadgeBackgroundColor({ color: "#ffa8a8" });
+          await chrome.action.setBadgeText({ text: "error" });
+        } finally {
+          await new Promise((resolve) => setTimeout(resolve, 2_000));
+          await chrome.action.setBadgeText({ text: "" });
+        }
+      }
+      run();
+    }
+  );
+});
+
+export {};
